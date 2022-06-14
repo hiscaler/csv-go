c6c1e0d5aac8e8fa72a0d1bd5fc9b26c5c5666dd
hiscaler <hiscaler@gmail.com>
2022-06-14 19:54:06 +0800

    Enh: Added IsEmpty and IsBlack method for column

--- column.go
+++ column.go
@@ -15,18 +15,18 @@ type Column struct {
 	NewValue      string
 }
 
-func (v *Column) TrimSpace() *Column {
-	v.NewValue = strings.TrimSpace(v.OriginalValue)
-	return v
+func (c *Column) TrimSpace() *Column {
+	c.NewValue = strings.TrimSpace(c.OriginalValue)
+	return c
 }
 
-func (v *Column) Do(f func(s string) string) *Column {
-	v.NewValue = f(v.OriginalValue)
-	return v
+func (c *Column) Do(f func(s string) string) *Column {
+	c.NewValue = f(c.OriginalValue)
+	return c
 }
 
-func (v Column) String() string {
-	return v.NewValue
+func (c Column) String() string {
+	return c.NewValue
 }
 
 func getValue(v Column, defaultValue ...string) string {
@@ -45,56 +45,64 @@ func cleanNumber(s string) string {
 	return s
 }
 
-func (v Column) ToInt(defaultValue ...string) (int, error) {
-	return strconv.Atoi(cleanNumber(getValue(v, defaultValue...)))
+func (c Column) IsEmpty() bool {
+	return c.NewValue == ""
 }
 
-func (v Column) ToInt8(defaultValue ...string) (int8, error) {
-	i, err := strconv.ParseInt(cleanNumber(getValue(v, defaultValue...)), 10, 8)
+func (c Column) IsBlack() bool {
+	return c.NewValue == "" || strings.TrimSpace(c.NewValue) == ""
+}
+
+func (c Column) ToInt(defaultValue ...string) (int, error) {
+	return strconv.Atoi(cleanNumber(getValue(c, defaultValue...)))
+}
+
+func (c Column) ToInt8(defaultValue ...string) (int8, error) {
+	i, err := strconv.ParseInt(cleanNumber(getValue(c, defaultValue...)), 10, 8)
 	if err != nil {
 		return 0, err
 	}
 	return int8(i), nil
 }
 
-func (v Column) ToInt16(defaultValue ...string) (int16, error) {
-	i, err := strconv.ParseInt(cleanNumber(getValue(v, defaultValue...)), 10, 16)
+func (c Column) ToInt16(defaultValue ...string) (int16, error) {
+	i, err := strconv.ParseInt(cleanNumber(getValue(c, defaultValue...)), 10, 16)
 	if err != nil {
 		return 0, err
 	}
 	return int16(i), nil
 }
 
-func (v Column) ToInt32(defaultValue ...string) (int32, error) {
-	i, err := strconv.ParseInt(cleanNumber(getValue(v, defaultValue...)), 10, 32)
+func (c Column) ToInt32(defaultValue ...string) (int32, error) {
+	i, err := strconv.ParseInt(cleanNumber(getValue(c, defaultValue...)), 10, 32)
 	if err != nil {
 		return 0, err
 	}
 	return int32(i), nil
 }
 
-func (v Column) ToInt64(defaultValue ...string) (int64, error) {
-	return strconv.ParseInt(cleanNumber(getValue(v, defaultValue...)), 10, 64)
+func (c Column) ToInt64(defaultValue ...string) (int64, error) {
+	return strconv.ParseInt(cleanNumber(getValue(c, defaultValue...)), 10, 64)
 }
 
-func (v Column) ToFloat32(defaultValue ...string) (float32, error) {
-	i, err := strconv.ParseFloat(cleanNumber(getValue(v, defaultValue...)), 32)
+func (c Column) ToFloat32(defaultValue ...string) (float32, error) {
+	i, err := strconv.ParseFloat(cleanNumber(getValue(c, defaultValue...)), 32)
 	if err != nil {
 		return 0, err
 	}
 	return float32(i), nil
 }
 
-func (v Column) ToFloat64(defaultValue ...string) (float64, error) {
-	return strconv.ParseFloat(cleanNumber(getValue(v, defaultValue...)), 64)
+func (c Column) ToFloat64(defaultValue ...string) (float64, error) {
+	return strconv.ParseFloat(cleanNumber(getValue(c, defaultValue...)), 64)
 }
 
-func (v Column) ToBool(defaultValue ...string) (bool, error) {
-	return strconv.ParseBool(strings.ToLower(getValue(v, defaultValue...)))
+func (c Column) ToBool(defaultValue ...string) (bool, error) {
+	return strconv.ParseBool(strings.ToLower(getValue(c, defaultValue...)))
 }
 
-func (v Column) ToTime(layout string, loc *time.Location, defaultValue ...string) (time.Time, error) {
-	s := getValue(v, defaultValue...)
+func (c Column) ToTime(layout string, loc *time.Location, defaultValue ...string) (time.Time, error) {
+	s := getValue(c, defaultValue...)
 	if s == "" {
 		return time.Time{}, errors.New("is empty string")
 	}
