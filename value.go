8dafea4705e8b33d8933b007604b9c1c4e630a9f
hiscaler <hiscaler@gmail.com>
2022-06-14 15:29:28 +0800

    Add: Added Value.ToTime method

--- value.go
+++ value.go
@@ -1,8 +1,10 @@
 package csv
 
 import (
+	"errors"
 	"strconv"
 	"strings"
+	"time"
 )
 
 type Value struct {
@@ -27,26 +29,30 @@ func (v Value) String() string {
 	return v.NewValue
 }
 
-func (v Value) ToInt(defaultValue ...string) (int, error) {
+func getValue(v Value, defaultValue ...string) string {
 	s := v.String()
 	if s == "" && len(defaultValue) > 0 {
 		s = defaultValue[0]
 	}
-	return strconv.Atoi(s)
+	return s
+}
+
+func (v Value) ToInt(defaultValue ...string) (int, error) {
+	return strconv.Atoi(getValue(v, defaultValue...))
 }
 
 func (v Value) ToFloat64(defaultValue ...string) (float64, error) {
-	s := v.String()
-	if s == "" && len(defaultValue) > 0 {
-		s = defaultValue[0]
-	}
-	return strconv.ParseFloat(s, 64)
+	return strconv.ParseFloat(getValue(v, defaultValue...), 64)
 }
 
 func (v Value) ToBool(defaultValue ...string) (bool, error) {
-	s := v.String()
-	if s == "" && len(defaultValue) > 0 {
-		s = defaultValue[0]
+	return strconv.ParseBool(getValue(v, defaultValue...))
+}
+
+func (v Value) ToTime(layout string, loc *time.Location, defaultValue ...string) (time.Time, error) {
+	s := getValue(v, defaultValue...)
+	if s == "" {
+		return time.Time{}, errors.New("is empty string")
 	}
-	return strconv.ParseBool(s)
+	return time.ParseInLocation(s, layout, loc)
 }
